18e9019cc6ff5f21d0e87dff11ee4ee4276f73b3
Obiora Igboanusi <cwertlinks@gmail.com>
2021-05-20 17:19:53 +0100

    add form preview

--- app.js
+++ app.js
@@ -1,66 +1,94 @@
 "use strict";
-const defaultSlide = document.querySelector(".default-slide");
+window.addEventListener("DOMContentLoaded", () => {
+  const defaultSlide = document.querySelector(".default-slide");
+  const form = document.querySelector("form");
+  const nextBtns = document.getElementsByClassName("next");
+  const prevBtns = document.getElementsByClassName("prev");
+  const progressBars = document.getElementsByClassName("step");
+ 
+  const radioElements = document.querySelectorAll("input[type=radio]");
 
-const nextBtns = document.getElementsByClassName("next");
-const prevBtns = document.getElementsByClassName("prev");
-const progressBars = document.getElementsByClassName("step");
-const inputFields = document.getElementsByClassName("field");
-const formFields = document.getElementsByClassName("form-field");
+  const confirmSubmitBtn = document.getElementById("confirm-submit");
+  const returnToEdithBtn = document.getElementById("return-to-edith");
 
-let currentSlide = 1;
+  let currentSlide = 1;
 
-// Handle next buttons click
-console.log(nextBtns.length)
-for (let i = 0; i <= nextBtns.length - 1; i++) {
-  nextBtns[i].addEventListener("click", (e) => {
-    if (i + 1 === nextBtns.length) {
+  // Handle next buttons click
+  for (let i = 0; i <= nextBtns.length - 1; i++) {
+    nextBtns[i].addEventListener("click", (e) => {
+      if (i === nextBtns.length - 1) {
+        e.preventDefault();
+
+       // preview form at the end of the slide
+        progressForward();
+        previewForm();
+      } else {
+        // slide forward
+        e.preventDefault();
+        slideForward(i);
+        progressForward();
+      }
+    });
+  }
+// handle previous button click
+  for (let i = 0; i <= prevBtns.length - 1; i++) {
+    prevBtns[i].addEventListener("click", (e) => {
       e.preventDefault();
+      slideBackward(i);
+      progressBackward();
+    });
+  }
 
-      //submit form
-      alert("Success!!! You have submited the form");
-    } else {
-      //slide forward
-      e.preventDefault();
-      slideForward(i);
-      progressForward()
-    }
-  });
-}
-for (let i = 0; i <= prevBtns.length - 1; i++) {
-  prevBtns[i].addEventListener("click", (e) => {
+  confirmSubmitBtn.addEventListener("click", (e) => {
     e.preventDefault();
-    slideBackward(i);
-    progressBackward()
+    location.replace("submit.html");
   });
-}
-console.log(inputFields)
-for (let i = 0; i <= inputFields.length; i++) {
-  // if (inputFields[i].value) {
-  //   formFields[i].classList.add('active')
-  // } else {
-  //   formFields[i].classList.remove('active')
-  // }
-}
+  returnToEdithBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    progressBackward();
+    returnToEdithForm();
+  });
+  function slideForward(slideIndex) {
+    defaultSlide.style.marginLeft = calcMarginLeftForward(slideIndex);
+  }
+  function slideBackward(slideIndex) {
+    defaultSlide.style.marginLeft = calcMarginLeftBack(slideIndex);
+  }
 
-function slideForward(slideIndex) {
-  defaultSlide.style.marginLeft = calcMarginLeftForward(slideIndex);
-}
-function slideBackward(slideIndex) {
-  defaultSlide.style.marginLeft = calcMarginLeftBack(slideIndex);
-}
-console.log(((100/600)*100))
-function calcMarginLeftForward(slideIndex) {
-  let num = slideIndex + 1;
-  return `-${ ((100/600)*100) * num}%`;
-}
-function calcMarginLeftBack(slideIndex) {
-  return `-${((100/600)*100) * slideIndex}%`;
-}
-function progressForward() {
-  progressBars[currentSlide - 1].classList.add("active");
-  currentSlide += 1
-}
-function progressBackward() {
-  progressBars[currentSlide - 2].classList.remove("active");
-  currentSlide -= 1
-}
+  function calcMarginLeftForward(slideIndex) {
+    let num = slideIndex + 1;
+    return `-${20 * num}%`;
+  }
+  function calcMarginLeftBack(slideIndex) {
+    return `-${20 * slideIndex}%`;
+  }
+  function progressForward() {
+    progressBars[currentSlide - 1].classList.add("active");
+    currentSlide += 1;
+  }
+  function progressBackward() {
+    progressBars[currentSlide - 2].classList.remove("active");
+    currentSlide -= 1;
+  }
+  function previewForm() {
+    form.classList.remove("slide-mode");
+    form.classList.add("preview-mode");
+    defaultSlide.style.marginLeft = "0%";
+
+//disable radio buttons
+    for (let i = 0; i < radioElements.length; i++) {
+      radioElements[i].setAttribute("disabled", "true");
+    }
+  }
+
+  function returnToEdithForm() {
+    form.classList.add("slide-mode");
+    form.classList.remove("preview-mode");
+    defaultSlide.style.marginLeft = "-80%";
+
+//enable radio buttons
+    for (let i = 0; i < radioElements.length; i++) {
+      radioElements[i].setAttribute("disabled", "false");
+    }
+  }
+});
