c1b5a458cc75fceb328dafc8b58d347c94fe5be8
Obiora Igboanusi <cwertlinks@gmail.com>
2021-05-25 03:40:36 +0100

    implement add features

--- app.js
+++ app.js
@@ -85,32 +85,183 @@ window.addEventListener("DOMContentLoaded", () => {
 
     //display all slides
     slides.forEach((slide) => {
-        slide.style.display = "block";
+      slide.style.display = "block";
     });
-  
 
     //disable radio buttons
     for (let i = 0; i < radioElements.length; i++) {
       radioElements[i].setAttribute("disabled", "true");
     }
   }
-  
+
   function returnToEdithForm() {
     form.classList.add("slide-mode");
     form.classList.remove("preview-mode");
 
     slides.forEach((slide, index) => {
-      if (index === currentSlide-1) {
+      if (index === currentSlide - 1) {
         slide.style.display = "block";
       } else {
         slide.style.display = "none";
       }
     });
-  
 
     //enable radio buttons
     for (let i = 0; i < radioElements.length; i++) {
       radioElements[i].setAttribute("disabled", "false");
     }
   }
+// rental income add
+  const rentalIncome = document.querySelector("#rental-income");
+  const rentalIncomeEntries = document.querySelector("#rental-income-entries");
+  const rentalIncomeAdd = document.querySelector(".rental-income-add");
+  const totalRentalIncome = document.querySelector("#total-rental-income");
+  const rentalIncomeList = [];
+
+  rentalIncomeAdd.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (rentalIncome.value && !isNaN(rentalIncome.value)) {
+      rentalIncomeList.push(parseInt(rentalIncome.value));
+      rentalIncomeEntries.insertAdjacentHTML(
+        "beforeend",
+        `<li>Property ${rentalIncomeList.length}: ${rentalIncome.value}</li>`
+      );
+  
+      rentalIncome.value = "";
+      totalRentalIncome.textContent = rentalIncomeList.reduce(
+        (a, b) => a + b,
+        0
+      );
+    }
+  });
+
+  // personal loan add
+  const personalLoanEntries = document.querySelector(".personal-loan-entries");
+  const personalLoanBank = document.querySelector(".personal-loan .bank-name");
+  const personalLoanOutstanding = document.querySelector(
+    ".personal-loan .loan-outstanding"
+  );
+  const personalLoanInstallment = document.querySelector(
+    ".personal-loan .monthly-installment"
+  );
+  const personalLoanAdd = document.querySelector(
+    ".personal-loan .add-new"
+  );
+  const personalLoanList = [];
+ 
+  personalLoanAdd.addEventListener('click', (e) => {
+    e.preventDefault()
+    
+    if (personalLoanBank.value &&  personalLoanOutstanding.value && personalLoanInstallment.value) {
+      personalLoanList.push({
+        bank: personalLoanBank.value,
+        outstanding: personalLoanOutstanding.value, installment: personalLoanInstallment.value
+      })
+      personalLoanEntries.insertAdjacentHTML('beforeend', AddTemplate(personalLoanBank.value, personalLoanOutstanding.value, personalLoanInstallment.value))
+      personalLoanBank.value = '';
+      personalLoanOutstanding.value='';
+     personalLoanInstallment.value=''
+    }
+  })
+  // home loan add
+  const homeLoanEntries = document.querySelector(".home-loan-display");
+  const homeLoanBank = document.querySelector(".home-loan .bank-name");
+  const homeLoanOutstanding = document.querySelector(
+    ".home-loan .loan-outstanding"
+  );
+  const homeLoanInstallment = document.querySelector(
+    ".home-loan .monthly-installment"
+  );
+  const homeLoanAdd = document.querySelector(
+    ".home-loan .add-new"
+  );
+  const homeLoanList = [];
+ 
+  homeLoanAdd.addEventListener('click', (e) => {
+    e.preventDefault()
+    
+    if (homeLoanBank.value &&  homeLoanOutstanding.value && homeLoanInstallment.value) {
+      homeLoanList.push({
+        bank: homeLoanBank.value,
+        outstanding: homeLoanOutstanding.value, installment: homeLoanInstallment.value
+      })
+      homeLoanEntries.insertAdjacentHTML('beforeend', AddTemplate(homeLoanBank.value, homeLoanOutstanding.value, homeLoanInstallment.value))
+      homeLoanBank.value = '';
+      homeLoanOutstanding.value='';
+     homeLoanInstallment.value=''
+    }
+  })
+  //  Credit Card add
+  const creditCardEntries = document.querySelector(".credit-card-display");
+  const creditCardBank = document.querySelector(".credit-card .bank-name");
+  const creditCardLimit = document.querySelector(
+    ".credit-card .card-limit"
+  );
+ 
+  const creaditCardAdd = document.querySelector(
+    ".credit-card .add-new"
+  );
+  const creditCardList = [];
+ 
+  creaditCardAdd.addEventListener('click', (e) => {
+    e.preventDefault()
+    if (creditCardLimit.value && creditCardBank.value) {
+      
+      creditCardList.push({
+        bank: creditCardBank.value,
+        limit: creditCardLimit.value
+      })
+      creditCardEntries.insertAdjacentHTML('beforeend', `
+      <li>${creditCardBank.value}
+        <ul>
+            <li>credit limit: ${creditCardLimit.value}</li>
+        </ul>
+      </li>
+      `)
+      creditCardBank.value = '';
+      creditCardLimit.value='';
+    }
+  })
+  
+  // home loan add
+  const autoLoanEntries = document.querySelector(".auto-loan-display");
+  const autoLoanBank = document.querySelector(".auto-loan .bank-name");
+  const autoLoanOutstanding = document.querySelector(
+    ".auto-loan .loan-outstanding"
+  );
+  const autoLoanInstallment = document.querySelector(
+    ".auto-loan .monthly-installment"
+  );
+  const autoLoanAdd = document.querySelector(
+    ".auto-loan .add-new"
+  );
+  const autoLoanList = [];
+ 
+  autoLoanAdd.addEventListener('click', (e) => {
+    e.preventDefault()
+    console.log(autoLoanBank)
+    if (autoLoanBank.value &&  autoLoanOutstanding.value && autoLoanInstallment.value) {
+      autoLoanList.push({
+        bank: autoLoanBank.value,
+        outstanding: autoLoanOutstanding.value, installment: autoLoanInstallment.value
+      })
+      autoLoanEntries.insertAdjacentHTML('beforeend', AddTemplate(autoLoanBank.value, autoLoanOutstanding.value, autoLoanInstallment.value))
+      autoLoanBank.value = '';
+      autoLoanOutstanding.value='';
+      autoLoanInstallment.value=''
+    }
+  })
+
+  function AddTemplate(bank, outstanding, installment) {
+    return (`
+      <li>${bank}
+        <ul>
+            <li>Outstandin: ${outstanding}</li>
+            <li>installment: ${installment}</li>
+        </ul>
+      </li>
+      `
+  )
+}
+
 });
